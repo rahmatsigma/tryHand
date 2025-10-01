55040e8b3b9d2625cfd2621d07c085db7098da66
rahmatsigma <rahmattillah079@gmail.com>
2025-10-01 23:07:35 +0700

    feat: confidence

--- deteksi_kamera.py
+++ deteksi_kamera.py
@@ -40,7 +40,7 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             
-            if confidence > 0.4:
+            if confidence > 0.3:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
